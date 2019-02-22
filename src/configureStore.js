e7956809b5598e73c062d28d0ded1527b26f9a2f
Salvador Aceves <saceves@socialdecode.com>
2019-02-21 21:04:31 -0600

    linting

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,4 +1,4 @@
-import "regenerator-runtime/runtime";
+import 'regenerator-runtime/runtime';
 import { createBrowserHistory } from 'history';
 import { applyMiddleware, compose, createStore } from 'redux';
 import { routerMiddleware } from 'connected-react-router';
